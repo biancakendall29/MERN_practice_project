f4ffe4c32d0998c44b3c430c18381d1dd126667d
Bianca Kendall <biancakendall29@gmail.com>
2023-03-29 18:25:29 +0100

    implement stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import '@babel/polyfill';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM ELEMENTS
 const loginForm = document.querySelector('.form--login');
@@ -61,3 +62,6 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 20);
